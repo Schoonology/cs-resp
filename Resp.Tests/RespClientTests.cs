8078e1d53df21c5d8d2076b79c04485cbdd7e829
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2014-02-21 20:13:28 -0500

    Bump version and add RespClientTests.

---
+++ Resp.Tests/RespClientTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Resp;
+
+namespace Resp.Tests
+{
+    [TestClass]
+    class RespClientTests
+    {
+        [TestMethod]
+        public void TestCreation()
+        {
+            RespClient client = new RespClient();
+        }
+    }
+}
